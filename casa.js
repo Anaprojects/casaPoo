5a2ef739392e8a1759a00eac814453c02bf4a16d
Ana Carolina <anacarolina.silva@onfly.com.br>
2024-01-26 16:15:45 -0300

    fiz uma istanciação

--- casa.js
+++ casa.js
@@ -7,32 +7,31 @@ class Casa{
     }
 
     //metodo Que muda a cor
-
     mudarCor(){
         console.log("casa pintada da cor desejada!😍")
     }
-
     //metodo que faxina
-
     limpaCasa(){
         console.log("casa limpada com sucesso!🧹")
     }
-
     //metodo que coloca lixo para fora
-
     colocarLixoParaFora(){
         console.log("lixo colocado para fora!🗑️")
     }
-
     //metodo que abre as janelas
-
     abrirAsJanelas(){
         console.log("janelas abertas🚪")
     }
-
     //metodos que abrem as portas
-
     abrirPortas(){
         console.log("janelasAbertas🪟")
     }
 }
+
+    // instanciação da classe
+     const minhaCasa = new Casa("rosa","suja","fechada","aberta");
+
+     console.log(minhaCasa.cor)
+     
+
+
